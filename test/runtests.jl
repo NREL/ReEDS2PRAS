a5570af912cf7b8762b51f789e8aaecd7b67d51c
Luke Lavin <llavin@el3.ib0.cm.hpc.nrel.gov>
2022-11-22 15:05:33 -0700

    newtest

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,12 @@ using ReEDS2PRAS
 using PRAS
 
 using Dates
+using CSV
+using DataFrames
 using Test
 using HDF5
 
 @testset "ReEDS2PRAS" begin
-    include("ercot/toyercot.jl")
     include("ercot/testlinesonly.jl")
+    include("ercot/toyercot.jl")
 end
