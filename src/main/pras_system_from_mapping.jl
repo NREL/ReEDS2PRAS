af0035239750fe51b7492e180e15c695893d2866
Luke Lavin <llavin@el3.ib0.cm.hpc.nrel.gov>
2022-11-22 14:31:27 -0700

    updateloadload

--- src/main/pras_system_from_mapping.jl
+++ src/main/pras_system_from_mapping.jl
@@ -11,30 +11,30 @@ function make_pras_system_from_mapping_info(ReEDSfilepath::String, Year::Int64)
     # Loading the necessary mapping files and data
     #######################################################
     # Load the mapping metadata JSON file
-    # @info "Fetching ReEDS case data to build PRAS System..."
+    @info "Fetching ReEDS case data to build PRAS System..."
 
-    # #load-related information
-    # load_info = h5read(joinpath(ReEDSfilepath,"load.h5"), "data") 
-    # load_data = load_info["block0_values"]
-    # regions = load_info["axis0"]
+    #load-related information
+    load_info = h5read(joinpath(ReEDSfilepath,"load.h5"), "data") 
+    load_data = load_info["block0_values"]
+    regions = load_info["axis0"]
     
-    # slicer = findfirst(isequal(Year), load_info["axis1_level0"])
-    # indices = findall(i->(i==(slicer-1)),load_info["axis1_label0"]) #I think b/c julia indexes from 1 we need -1 here
-    # #probably want some kind of assert/error here if indices are empty that states the year is invalid
-    # load_year = load_data[:,indices] #should be regionsX8760 if done right
-    # # println(typeof(load_year))
-    # # new_regions = PRAS.Regions{N,PRAS.MW}(regions, load_year);
+    slicer = findfirst(isequal(Year), load_info["axis1_level0"])
+    indices = findall(i->(i==(slicer-1)),load_info["axis1_label0"]) #I think b/c julia indexes from 1 we need -1 here
+    #probably want some kind of assert/error here if indices are empty that states the year is invalid
+    load_year = load_data[:,indices] #should be regionsX8760 if done right
+    # println(typeof(load_year))
+    # new_regions = PRAS.Regions{N,PRAS.MW}(regions, load_year);
     
-    # @info "Processing Areas in the mapping file..."
-    # # area_names = string.(mapping_data[!,"PlainInter"]);
-    # num_areas = length(regions); 
-    # N = size(load_year)[2]#DataFrames.nrow(load_data)
-    # region_load = Array{Int64,2}(undef,num_areas,N);
+    @info "Processing Areas in the mapping file..."
+    # area_names = string.(mapping_data[!,"PlainInter"]);
+    num_areas = length(regions); 
+    N = size(load_year)[2]#DataFrames.nrow(load_data)
+    region_load = Array{Int64,2}(undef,num_areas,N);
 
-    # for (idx,region) in enumerate(regions)
-    #     region_load[idx,:]=floor.(Int,load_year[idx,:]); #converts to int
-    # end
-    # new_regions = PRAS.Regions{N,PRAS.MW}(regions, region_load);
+    for (idx,region) in enumerate(regions)
+        region_load[idx,:]=floor.(Int,load_year[idx,:]); #converts to int
+    end
+    new_regions = PRAS.Regions{N,PRAS.MW}(regions, region_load);
     
     #lines
     @info "Processing lines..."
