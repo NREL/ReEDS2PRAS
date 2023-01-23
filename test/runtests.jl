ae30af51c73a818845ae59b4ad77e78c5803928e
Luke Lavin <llavin@el1.ib0.cm.hpc.nrel.gov>
2023-01-23 10:05:10 -0700

    correct bug for scenarios with no VSC converters

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,7 +11,8 @@ using TimeZones
 include("testutils.jl")
 
 @testset "ReEDS2PRAS" begin
-    include("ercot/ntpscenarios.jl")
+    # include("ercot/ntpscenarios.jl")
     # include("ercot/standardscenarios.jl")
     # include("ercot/toyercot.jl")
+    include("ercot/ntpscenarios_plot.jl")
 end
