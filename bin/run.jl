61e5db2668022303b653bf414e66de15e7fb6d6e
Patrick Brown <Patrick.Brown@nrel.gov>
2023-03-27 15:35:22 -0600

    revert logging changes

--- bin/run.jl
+++ bin/run.jl
@@ -6,8 +6,6 @@ include("../src/ReEDS2PRAS.jl")
 
 import PRAS
 import ArgParse
-import Logging
-import LoggingExtras
 
 function parse_commandline()
     """
@@ -16,37 +14,24 @@ function parse_commandline()
     s = ArgParse.ArgParseSettings()
 
     @ArgParse.add_arg_table s begin
-        "--reedspath"
-            help = "Path to ReEDS-2.0 folder"
+        "reeds_filepath"
+            help = "Location of ReEDS filepath where inputs, results, and " *
+                   "outputs are stored"
             required = true
-        "--reedscase"
-            help = "Path to ReEDS run (usually .../ReEDS-2.0/runs/{casename})"
-            required = true
-        "--solve_year"
+        "solve_year"
             help = "Year for the case being generated"
-            arg_type = Int
             required = true
-        "--weather_year"
-            help = "The year corresponding to the vg profiles"
-            arg_type = Int
+        "nems_path"
+            help = "Path to NEMS public resource database"
             required = true
-        "--output_filepath"
-            help = "The path for saving the final model. e.g. ./model.pras"
-            required = false
-        "--timesteps"
+        "timesteps"
             help = "Number of timesteps to use"
-            arg_type = Int
-            default = 8760
-            required = false
-        "--log_console"
-            help = "Indicate whether to log to console in addition to file"
-            arg_type = Int
-            default = 1
-            required = false
-        "--debug"
-            help = "Indicate whether to log debug-level messages"
-            arg_type = Int
-            default = 0
+            required = true
+        "weather_year"
+            help = "The year corresponding to the vg profiles"
+            required = true
+        "output_filepath"
+            help = "The path for saving the final model. e.g. ./model.pras"
             required = false
     end
     return ArgParse.parse_args(s)
@@ -55,41 +40,14 @@ end
 
 function main()
     parsed_args = parse_commandline()
-    # Set up logger
-    if ~isnothing(parsed_args["output_filepath"])
-        logfile = replace(parsed_args["output_filepath"], ".pras"=>".log")
-
-        if parsed_args["debug"] == 1
-            logfilehandle = LoggingExtras.MinLevelLogger(
-                LoggingExtras.FileLogger(logfile),
-                Logging.Debug)
-        else
-            logfilehandle = LoggingExtras.MinLevelLogger(
-                LoggingExtras.FileLogger(logfile),
-                Logging.Info)
-        end
-
-        if parsed_args["log_console"] == 1
-            logger = LoggingExtras.TeeLogger(
-                Logging.global_logger(),
-                logfilehandle
-            )
-        else
-            logger = logfilehandle
-        end
-
-        Logging.global_logger(logger)
-    end
-
     @info "Running reeds_to_pras with the follow inputs:"
     for (arg, val) in parsed_args
         @info "$arg  =>  $val"
     end
-    # Run ReEDS2PRAS
     pras_system = ReEDS2PRAS.reeds_to_pras(
-        parsed_args["reedscase"], parsed_args["solve_year"],
-        parsed_args["reedspath"], parsed_args["timesteps"],
-        parsed_args["weather_year"])
+        parsed_args["reeds_filepath"], parse(Int64, parsed_args["solve_year"]),
+        parsed_args["nems_path"], parse(Int64, parsed_args["timesteps"]),
+        parse(Int64, parsed_args["weather_year"]))
     if ~isnothing(parsed_args["output_filepath"])
         PRAS.savemodel(pras_system, parsed_args["output_filepath"])
     end
