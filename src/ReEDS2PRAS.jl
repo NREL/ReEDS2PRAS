7be2fc91fc0f1c75dc161181753daa0a7eead0ce
sdhulipa <suryachandan.dhulipala@nrel.gov>
2023-04-05 18:49:21 -0600

    Clean up module import, exports and move main function to a separate script

--- src/ReEDS2PRAS.jl
+++ src/ReEDS2PRAS.jl
@@ -5,12 +5,9 @@ Author: Luke Lavin, Surya Chandan Dhulipala, Brandon Norton Benton
 Email: luke.lavin@nrel.gov, suryachandan.dhulipala@nrel.gov, brandon.benton@nrel.gov
 """
 module ReEDS2PRAS
+
 # Exports
-export Generators
-export Thermal_Gen
-export VG_Gen
-export Region
-export Line
+export reeds_to_pras
 
 # Imports
 import CSV
@@ -35,73 +32,9 @@ include("models/utils.jl")
 
 # Utils
 include("utils/load_reeds.jl") 
+include("utils/create_pras.jl")
 
 # Main
-include("create_pras.jl")
-
-#runs ReEDS2PRAS
-"""
-    Generates a PRAS system from data in ReEDSfilepath
-
-    Parameters
-    ----------
-    ReEDSfilepath : String
-        Location of ReEDS filepath where inputs, results, and outputs are
-        stored
-    year : Int64
-        ReEDS solve year
-    reedspath : String
-        Path to EIA NEMS database
-    timesteps : Int
-        Number of timesteps
-    WEATHERYEAR : Int
-        The weather year for variable gen profiles and load
-
-    Returns
-    -------
-    PRAS.SystemModel
-        PRAS SystemModel struct with regions, interfaces, generators,
-        region_gen_idxs, storages, region_stor_idxs, generatorstorages,
-        region_genstor_idxs, lines, interface_line_idxs, timestamps
-
-"""
-function reeds_to_pras(
-    reedscase::String,
-    solve_year::Int64,
-    reedspath::String,
-    timesteps::Int,
-    weather_year::Int,
-)
-    # assume valid weather years as hardcode for now. These should eventually
-    # be read in from ReEDS
-    if weather_year ∉ [2007, 2008, 2009, 2010, 2011, 2012, 2013]
-        error(
-            "The weather year $weather_year is not a valid VG profile " *
-            "year. Should be an Int in 2007-2013 currently",
-        )
-    end
-    ReEDS_data_filepaths = ReEDSdatapaths(reedscase, solve_year)
-
-    @info "creating PRAS system objects..."
-    out = load_objects(
-        ReEDS_data_filepaths,
-        weather_year,
-        timesteps,
-        solve_year,
-        reedspath,
-        2007,
-    )
-    all_lines, regions, all_gens, storages_array, genstor_array = out
-    @info "...objects are created, writing to PRAS system"
-    return create_pras_system(
-        regions,
-        all_lines,
-        all_gens,
-        storages_array,
-        genstor_array,
-        timesteps,
-        weather_year,
-    )
-end
+include("reeds_to_pras.jl") 
 
 end
