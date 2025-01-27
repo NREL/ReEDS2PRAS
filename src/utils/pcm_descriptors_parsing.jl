b6fe60633e1572be689354ce8098d22447b1d37e
Lavin <llavin@nrel.gov>
2025-01-27 15:43:06 -0700

    add pcm defaults from r2x

---
+++ src/utils/pcm_descriptors_parsing.jl
@@ -0,0 +1,23 @@
+isjson = endswith(".json");
+
+inner_keys = ["avg_capacity_MW","mean_time_to_repair","forced_outage_rate"]
+
+function parse_pcm_default_descriptors(file::String)
+    if ~(isjson(file))
+        @error "PCM default input descriptors file provided is not in JSON format. Exiting..."
+    end
+
+    # pcm_default_input_dict = JSON.parsefile(file)
+    # pcm_default_inputs = Dict()
+    # for key in keys(pcm_default_input_dict)
+    #     push!(pcm_default_inputs, key => inner_key)
+    #     for inner_key in inner_keys
+    #         push!(
+    #             pcm_default_inputs[key],
+    #             key => inner_key => pcm_default_input_dict[key][inner_key],
+    #         )
+    #     end
+    # end
+    println("yay")
+    return JSON.parsefile(file)
+end
